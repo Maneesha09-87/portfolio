0c90aae2ad463897a8039c16e488d00b66699d64
Maneesha09-87 <ramapurammaneesha@gmail.com>
2024-03-02 22:22:35 +0530

    Learning JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+//alert("Hello Javascript")
+//To debug shift+ctrl+i
+let a = 20
+if(a > 5)
+{
+  console.log("hello");
+}
+else{
+    console.log("hello1");
+}
+let tables = 4 //4tables
+console.log(44); //44
+/* Multiline Comment 
+Semicolon is optional*/
+// what is the value of 1+1
+/* primitive values :
+1.string --->"It's" , console.log("super" + "Man") ,('1'+'1')
+2.number --->console.log(1+9),console.log(2*5)
+3.boolean -->true or false 
+5.undefined -->null
+6.symbol --->$,{},[]
+
+
+
+*/
