a957c60b51588b9837a78cf89a790c25db1eb086
Maneesha09-87 <ramapurammaneesha@gmail.com>
2024-03-04 20:25:19 +0530

    Learning JavaScript

--- index.js
+++ index.js
@@ -19,7 +19,107 @@ Semicolon is optional*/
 3.boolean -->true or false 
 5.undefined -->null
 6.symbol --->$,{},[]
-
-
-
 */
+//COMPARISION OPERATORS
+/*There are 4 types of Comparision operators
+greater than>
+lessthan<
+strictly equal // ===
+strictly unequal
+*/
+24>23 //true
+24>24 //false
+24>=24 //false
+
+24==24 //true
+
+24!==24 //false
+'24' === 24 //false
+'24' == 24 //true
+
+0 == false //true
+//objects are not equal
+const z= {ishavingfun: true}
+const v={ishavingfun: true}
+console.log(z==v)
+console.log(v==z)
+
+//Always false
+//undefined ---> null
+//string doesnt be value
+// ------if else
+/*if(light is red)
+{
+  stop walking
+}
+else {
+  continue walking
+} */
+// ------> if else if
+/* if(light is red)
+{
+  stop walking
+}
+else if(cars around){
+  stop walking
+}
+else if(yet another condition){
+  do another thing
+}
+else{
+  do final thing
+}
+*/
+
+//not operator
+
+//console.log(!22) //false
+
+//console.log(!false)
+
+//const str = ''
+/*if(!str){
+  console.log('hello')
+}
+else{
+  console.log('world')
+} */
+
+/*!123333 //false
+ !true //false
+ !num //true
+
+ // !! double negation
+ // !! converts truly value is true
+ // !! converts false value is false
+ !! 'pandas are adorable' //true
+ !! '' //false
+
+ //undefined is value that indicates an absence of value
+
+ //when it is not explicity defined
+ 
+ /*let a 
+
+ console.log(a);
+ */
+//null is value that is used to indicate 'nothing'  , need to explicity set a value to be null
+//const z
+/*{
+  fname:'z',
+  mname:null,
+}
+// NULL == UNDEFINED ---->FALSE
+*/
+ 
+//BOM ---->collection of objects that allow js to interact with the browser window and other browser functionalities
+
+/*BROWSER OBJECT MODEL
+  window. ----->to use properties
+  console.log(window)---->properties
+*/
+
+//DOM
+/* DOCUMENT OBJECT MODEL
+dom refers to html to access and update the content and style of a document
+ */
